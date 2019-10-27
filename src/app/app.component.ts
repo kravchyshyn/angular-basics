40f3a15a252232851de867bf90f7f897bae44b86
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-27 23:45:02 +0200

    [Feature] Section 3, lessons 21-29

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+    title: string
+    text: string
+    id?: number
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+    posts: Post[] = [
+        // {title: 'Хочу вивчити Angular компоненти', text: 'Я все ще вчу компоненти', id: 1},
+        {title: 'Наступний блок', text: 'Буде про директиви та про пайпи бла бла блаі', id: 2}
+    ]
+
+    updatePosts(post: Post) {
+        this.posts.unshift(post)
+    }
+
+    removePost(id: number) {
+        this.posts = this.posts.filter(post => post.id !== id)
+    }
+
 }
