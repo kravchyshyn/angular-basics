89d4804f2169b58eb6e5a58009dd6791a7fee974
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-27 15:45:00 +0200

    [Feature]: initial commit for Section 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
-import {Post4Component} from './post4/post4.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent,
-    Post4Component
   ],
   imports: [
     BrowserModule,
