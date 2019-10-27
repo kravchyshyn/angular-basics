42f8aea8ebf01a9addee38eda95ee08e82f205c6
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-27 23:50:25 +0200

    [Feature] initial commit for Section 4 - Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,9 @@
 import {Component} from '@angular/core';
 
-export interface Post {
-    title: string
-    text: string
-    id?: number
-}
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    posts: Post[] = [
-        // {title: 'Хочу вивчити Angular компоненти', text: 'Я все ще вчу компоненти', id: 1},
-        {title: 'Наступний блок', text: 'Буде про директиви та про пайпи бла бла блаі', id: 2}
-    ]
-
-    updatePosts(post: Post) {
-        this.posts.unshift(post)
-    }
-
-    removePost(id: number) {
-        this.posts = this.posts.filter(post => post.id !== id)
-    }
-
 }
