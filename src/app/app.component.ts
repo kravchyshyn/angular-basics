12a22cdaa71b6f1cc515a3711392925e33063665
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-29 01:56:48 +0200

    [Fix] Initial commit for services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,66 +12,6 @@ export interface Post {
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
-    p: Promise<string> = new Promise<string>(resolve => {
-        setTimeout(()=> {
-            resolve('Promise resolve')
-        }, 4000)
-    })
+export class AppComponent{
 
-    date$: Observable<Date> = new Observable<Date>(obs =>{
-        setInterval(() => {
-            obs.next(new Date())
-        }, 1000)
-    })
-
-    // date: Date
-    //
-    ngOnInit(): void {
-        // this.date$.subscribe( date => {
-        //     this.date = date
-        // })
-    }
 }
-
-// export class AppComponent {
-//
-//     search = ''
-//     searchField = 'title'
-//
-//     posts: Post[] = [
-//         {title:'Beer', text:'The best 1beer in the world'},
-//         {title:'Bread', text: 'The best 1bread in the world'},
-//         {title:'Js', text:'The best 1language in the world'}
-//     ]
-//
-//     addPost() {
-//         this.posts.unshift({
-//             title: 'Angular new post',
-//             text:'the most unreal post'
-//         })
-//     }
-// }
-//
-
-
-// export class AppComponent {
-    // e: number = Math.E
-    //
-    // str: string = 'Hello World'
-    //
-    // date: Date = new Date()
-    //
-    // float: number = 0.42
-    //
-    // obj = {
-    //     a: 1,
-    //     b : {
-    //         c: 2,
-    //         d: {
-    //             e: 3,
-    //             f: 4
-    //         }
-    //     }
-    // }
-// }
