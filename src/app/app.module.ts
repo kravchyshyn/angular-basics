2d245b2684136f0744c8652a8d8d1a725b263bf3
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-29 14:40:20 +0200

    [Fix] Create initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {AppCounterService} from "./services/app-counter.service";
-import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
   ],
   imports: [
     BrowserModule,
       FormsModule
   ],
-  providers: [
-      AppCounterService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
