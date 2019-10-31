1ecc2525532a400d94e3e968ee0816dbd6e58800
Vitalii Kravchyshyn <vitalik1991ua@gmail.com>
2019-10-31 17:12:32 +0200

    [Feature] Routing create base template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {Todo, TodoService} from "./todo.service";
+import {Component} from '@angular/core'
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-    todos: Todo[] = []
-    newTodoTitle: string = '';
-
-    loading = false
-    error = ''
-
-    constructor(private todoService: TodoService) {}
-
-    ngOnInit(): void {
-        this.fetchTodos()
-    }
-
-    addTodo() {
-        if (!this.newTodoTitle.trim()) {
-            return false;
-        }
-
-        const newTodo: Todo = {
-            title: this.newTodoTitle,
-            completed: false
-        }
-
-        this.todoService.addTodo(newTodo)
-            .subscribe(response => {
-                this.todos.unshift(response)
-                this.newTodoTitle = ''
-            })
-    }
-
-    fetchTodos() {
-        this.loading = true
-            this.todoService.fetchTodos()
-            .subscribe(todos => {
-                this.todos = todos
-                this.loading = false
-            }, error => {
-                this.error = error.message
-            })
-    }
-
-    removeTodo(id:number) {
-        this.todoService.removeTodo(id)
-            .subscribe(() => {
-                this.todos = this.todos.filter(todo => todo.id !== id)
-            })
-    }
-
-    completeTodo(id: number) {
-        this.todoService.completeTodo(id)
-            .subscribe((response) => {
-                this.todos.find(todo => todo.id === response.id).completed = true
-            })
-    }
+export class AppComponent {
+  constructor() {}
 }
+
